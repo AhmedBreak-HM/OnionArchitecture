d9af0ddae834334d08553fa00f63adce630f5c63
ahmed break <ahmed_break8@yahoo.com>
2021-01-04 18:08:03 +0200

    Add project files.

---
+++ Core/Entities/Portfilio.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Portfilio : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImgeUrl { get; set; }
+    }
+}
