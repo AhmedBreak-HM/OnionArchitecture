da5448c558d3bcb9279e23e9cf92b68ec024201f
ahmed break <ahmed_break8@yahoo.com>
2021-01-05 13:20:53 +0200

    implement Repository-UnitOfWork DesignPattern

---
+++ Core/Interfaces/IGenircRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenircRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GettAll();
+
+        Task<T> GetById(object id);
+
+        Task Add(T entity);
+
+        void Update(T entity);
+
+        Task Remove(object id);
+    }
+}
---
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repository;
+using System.Threading.Tasks;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+        private readonly DataContext _context;
+        private IGenircRepository<T> _entity;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IGenircRepository<T> Entity
+        {
+            get
+            {
+                return _entity ??= new GenericRepository<T>(_context);
+            }
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
